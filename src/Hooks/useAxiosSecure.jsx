06c51adbc1ba6c02bddb437ef2026156c414c29d
alihasna32 <muktermukter55@gmail.com>
2026-05-04 01:08:26 +0600

    file update

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 import { UseAuth } from "./UseAuth";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://zapshift-server-8ne3.onrender.com/",
 });
 
 const useAxiosSecure = () => {
